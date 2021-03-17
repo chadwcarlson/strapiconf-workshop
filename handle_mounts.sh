0968504f704c67f17e3748ee1e7fc66a65c93076
chadcarlson <chad.carlson@platform.sh>
2021-03-17 14:44:15 -0400

    Clean up script.

--- handle_mounts.sh
+++ handle_mounts.sh
@@ -13,6 +13,15 @@
 
 MOUNT_TMP=platformsh-mounts
 
+# This script can get caught on the conditions that 1) a directory doesn't exist, or 2) doesn't contain files. This runs that check.
+directory_check(){
+    if [ -d $1 ]; then
+        if [ "$(ls $1)" ]; then 
+            echo "true"
+        fi
+    fi
+}
+
 # Hidden mounts require a preceding slash (i.e. `/.next`). This function just removes it to make the path work right.
 #   See: https://docs.platform.sh/configuration/app/storage.html#why-cant-i-mount-a-hidden-folder
 clean_mount_defn(){
@@ -24,14 +33,12 @@ clean_mount_defn(){
 #   as a mount to the tmp directory MOUNT_TMP.
 stage_files() {
     MOUNT=`clean_mount_defn $1`
-    # Create the tmp directory (MOUNT_TMP) on the first pass only. (/app/platformsh-mounts on Platform.sh)
-    if [ -d $PLATFORM_APP_DIR/$MOUNT_TMP ]; then
-        mkdir $PLATFORM_APP_DIR/$MOUNT_TMP
-    fi 
-    # Duplicate the mount directory in MOUNT_TMP. 
-    mkdir -p $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp
-    # Move its files.
-    mv $PLATFORM_APP_DIR/$MOUNT/* $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp
+    if [ "$(directory_check $PLATFORM_APP_DIR/$MOUNT)" ]; then
+        # Duplicate the mount directory in MOUNT_TMP. 
+        mkdir -p $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp
+        # Move its files.
+        mv $PLATFORM_APP_DIR/$MOUNT/* $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp
+    fi
 }
 
 # During the deploy hook, this function moves files committed to the same directory as one defined in `.platform.app.yaml`
@@ -39,7 +46,8 @@ stage_files() {
 #   deploy time.
 restore_files() {
     MOUNT=`clean_mount_defn $1`
-    if [ -d $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp ]; then 
+    # if [ -d $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp ]; then 
+    if [ "$(directory_check $PLATFORM_APP_DIR/$MOUNT_TMP/$MOUNT-tmp)" ]; then
         # Clean up files in mount so it's up to date with what we're moving over. 
         rm -r $PLATFORM_APP_DIR/$MOUNT/*
         # Restore the directory's files.
@@ -55,6 +63,10 @@ run() {
         _jq() {
             # Build hook. The $PLATFORM_BRANCH environment variable is not available in build containers.
             if [ -z "${PLATFORM_BRANCH}" ]; then
+                # Create the tmp directory (MOUNT_TMP) on the first pass only. (/app/platformsh-mounts on Platform.sh)
+                if [ -d $PLATFORM_APP_DIR/$MOUNT_TMP ]; then
+                    mkdir $PLATFORM_APP_DIR/$MOUNT_TMP
+                fi 
                 stage_files $mount
             # Deploy hook.
             else
